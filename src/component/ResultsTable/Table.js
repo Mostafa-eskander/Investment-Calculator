c00b99fd84bc1e1b41c235d7df73a9768a1ae5ee
Mostafa-eskander <mosgadeskander@gmail.com>
2026-02-17 14:04:20 +0200

    first commit

---
+++ src/component/ResultsTable/Table.js
@@ -0,0 +1,48 @@
+import classes from './Table.module.css';
+
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2
+});
+
+export default function TablePage({results,initialInvestment}) {
+  return(
+        <div className={classes.tableContainer}>
+          <table className={classes.result}>
+            <thead>
+              <tr>
+                <th>Year</th>
+                <th>Total Savings</th>
+                <th>Interest (Year)</th>
+                <th>Total Interest</th>
+                <th>Invested Capital</th>
+              </tr>
+            </thead>
+            <tbody>
+              {results.map((result) => {
+                return(
+                  <tr key={result.year}>
+                    <td>{result.year}</td>
+                    <td>{formatter.format(result.savingsEndOfYear)}</td>
+                    <td>{formatter.format(result.yearlyInterest)}</td>
+                    <td>
+                      {formatter.format(
+                        result.savingsEndOfYear - 
+                        initialInvestment - 
+                        result.yearlyContribution * result.year)}
+                    </td>
+                    <td>
+                      {formatter.format(
+                        initialInvestment + 
+                        result.yearlyContribution * result.year)}
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+    )
+}
